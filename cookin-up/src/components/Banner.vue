7c3801f67930bbcf6b343290340aee9397c0bc2f
HugoFalci <hugo.flc.ribeiro@gmail.com>
2024-05-21 13:21:49 -0300

    Aula 2

--- cookin-up/src/components/Banner.vue
+++ cookin-up/src/components/Banner.vue
@@ -2,17 +2,21 @@
     <header class="banner">
       <div>
         <img src="../assets/imagens/logo.svg" alt="Logo do cookin' up" class="logo">
+        
         <p class="cabecalho-lg frase-cabecalho">
           <span class="texto-verde">Um banquete de ideias para</span>
           despertar o chef que há em você!
         </p>
+
         <p class="subtitulo-lg">
           Explore novas receitas todos os dias com os ingredientes que estão ao seu alcance!
         </p>
+
       </div>
       <img src="../assets/imagens/foto-banner.png"
         alt="Foto de uma mulher cozinhando com uma bacia de cidro nas mãos. ela está sorrindo e está apoiando um celular entre seu rosto e ombro"
         class="foto-banner">
+
     </header>
   </template>
 
